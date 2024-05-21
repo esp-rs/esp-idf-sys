7abca6b2a883247e445dbce160470327910c8749
Frederick Vollbrecht <frederick@vollbrecht.cc>
2024-05-21 15:41:27 +0200

    🗑️ cleanup deprecated idf 4.3 related code

--- src/patches.rs
+++ src/patches.rs
@@ -3,12 +3,9 @@
 mod atexit;
 #[cfg(feature = "std")]
 mod lstat;
-#[cfg(all(feature = "std", esp_idf_version = "4.3"))]
-mod pthread_rwlock;
 
 #[allow(dead_code)]
 pub struct PatchesRef(
-    *mut core::ffi::c_void,
     *mut core::ffi::c_void,
     *mut core::ffi::c_void,
 );
@@ -16,11 +13,6 @@ pub struct PatchesRef(
 /// A hack to make sure that the rwlock implementation is linked to the final executable
 /// Call this function once e.g. in the beginning of your main function
 pub fn link_patches() -> PatchesRef {
-    #[cfg(all(feature = "std", esp_idf_version = "4.3"))]
-    let rwlock = pthread_rwlock::link_patches();
-    #[cfg(not(all(feature = "std", esp_idf_version = "4.3")))]
-    let rwlock = core::ptr::null_mut();
-
     #[cfg(feature = "std")]
     let lstat = lstat::link_patches();
     #[cfg(not(feature = "std"))]
@@ -28,5 +20,5 @@ pub fn link_patches() -> PatchesRef {
 
     let atexit = atexit::link_patches();
 
-    PatchesRef(rwlock, lstat, atexit)
+    PatchesRef(lstat, atexit)
 }
